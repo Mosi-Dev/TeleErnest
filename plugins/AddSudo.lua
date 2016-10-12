42c25fd4e3f52c5fc7caf1e653895c13726b5e8d
Ernest TEAM <Ernest_TEAM@yahoo.com>
2016-10-12 14:38:07 -0700

    Create AddSudo.lua

---
+++ plugins/AddSudo.lua
@@ -0,0 +1,37 @@
+-- @Ernest_TM
+do
+local function callback(extra, success, result)
+vardump(success)
+vardump(result)
+end
+local function run(msg, matches)
+local idsuodo = redis:get('bot:idsudo')
+local user = idsuodo
+if matches[1] == "addsudo" or matches[1] == "دعوت سودو" then
+user = 'user#id'..user
+end
+if is_owner(msg) then
+    if msg.from.username ~= nil then
+       local username = redis:get('bot:usernamesudo')
+      if string.find(msg.from.username , username) then
+          return "ℹ️سازنده هم اکنون در گروه است"
+          end
+if msg.to.type == 'channel' or 'chat' then
+local channel = 'channel#id'..msg.to.id
+chat_add_user(chat, user, ok_cb, false)
+channel_invite(channel, user, ok_cb, false)
+return "ℹ️درحال دعوت صاحب ربات برای حل مشکل شما..."
+end
+end
+end
+end
+return {
+patterns = {
+"^[#!/]([Aa]ddsudo)$",
+"^(دعوت سودو)$"
+},
+run = run
+}
+end
+
+-- @Ernest_TM
