30a06eb75c3ec8e3f3909d5018cdff01a7f1b048
Ernest TEAM <Ernest_TEAM@yahoo.com>
2016-10-12 09:10:23 -0700

    Update Stats.lua

--- plugins/Stats.lua
+++ plugins/Stats.lua
@@ -91,7 +91,7 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'roonx' then -- Put everything you like :)
+  if matches[1]:lower() == 'ernest' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
     savelog(msg.to.id, name.." ["..msg.from.id.."] used /teleseed ")
@@ -121,7 +121,7 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "roonx" then -- Put everything you like :)
+    if matches[2] == "ernest" then -- Put everything you like :)
       if not is_admin1(msg) then
         return "For admins only !"
       else
@@ -143,8 +143,8 @@ return {
     "^[#!/]([Ss]tats)$",
     "^[#!/]([Ss]tatslist)$",
     "^[#!/]([Ss]tats) (group) (%d+)",
-    "^[#!/]([Ss]tats) ([Rr]oonx)",
-	"^[#!/]([Rr]oonx)"
+    "^[#!/]([Ss]tats) ([Ee]rnest)",
+	"^[#!/]([Ee]rnest)"
     }, 
   run = run
 }
